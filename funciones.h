5cbce51173aca8302abb317e84a4679ec84e17b2
Altobert <alberto.sanmartin@usach.cl>
2022-01-26 13:00:32 -0300

    Programas

---
+++ funciones.h
@@ -0,0 +1,6 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void sumar(int num);
+
+#endif
---
+++ sumaNumeros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sumar(){
+
+}
+
+int main(){
+
+    int num = 0;
+    printf("Suma de los n primeros numeros");
+    scanf("%d", &num);
+    printf("%d",num);
+
+    return 0;
+}
